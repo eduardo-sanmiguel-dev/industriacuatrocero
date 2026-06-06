a589e6f99dcbfb827c8cb964a3efbcc4b8501d79
Eduardo San Miguel <eduardo.sanmiguel.dev@gmail.com>
2026-06-05 21:26:59 -0600

    fix(auth): update JWT secret retrieval to use env module

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -3,14 +3,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './services/auth.service';
 import { CoreModule } from '../core/core.module';
+import { env } from '../../env';
 
 @Module({
   imports: [
     CoreModule,
     // 🔑 Configuración del motor de firmas de tokens JWT para tu plataforma SaaS
     JwtModule.register({
-      secret:
-        process.env.JWT_SECRET || 'llave_secreta_super_segura_de_desarrollo', // Usa variables de entorno en producción
+      secret: env.JWT_SECRET,
       signOptions: {
         expiresIn: '8h', // Tiempo de vida estándar para una jornada laboral corporativa
       },
