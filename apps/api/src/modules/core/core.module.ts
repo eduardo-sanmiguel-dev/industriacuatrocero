0512c33a1db00a48afba61a1b4a267b15551aa30
Eduardo San Miguel <eduardo.sanmiguel.dev@gmail.com>
2026-06-08 19:36:02 -0600

    feat(core): add TenantBrand entity to TypeOrmModule imports

--- apps/api/src/modules/core/core.module.ts
+++ apps/api/src/modules/core/core.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { CoreService } from './core.service';
 import { CoreController } from './core.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tenant, User, Country, SystemAccount } from './entities';
+import { Tenant, User, Country, SystemAccount, TenantBrand } from './entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tenant, User, Country, SystemAccount])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Tenant,
+      User,
+      Country,
+      SystemAccount,
+      TenantBrand,
+    ]),
+  ],
   controllers: [CoreController],
   providers: [CoreService],
   exports: [TypeOrmModule],
