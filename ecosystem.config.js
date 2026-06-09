ef682df99cf00abe39e9540511c37ea28a1e0c69
Eduardo San Miguel <eduardo.sanmiguel.dev@gmail.com>
2026-06-09 14:03:14 -0600

    feat: create ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "industriacuatrocero", // Nombre identificador del proceso en PM2
+      script: "./apps/api/dist/main.js", // 🚀 Ejecuta directamente el JavaScript puro de NestJS compilado
+      cwd: "./apps/api", // Directorio de ejecución de la API
+      instances: 1, // 🔒 FIJO: Una sola instancia corriendo en el servidor
+      exec_mode: "fork", // 🚀 MODO FORK: Hilo único directo, sin clúster ni duplicación de backend
+      watch: false, // Desactivado para producción para evitar reinicios por logs
+      max_memory_restart: "1G", // Protección defensiva si ocurre una fuga de memoria
+
+      // 📈 LOGS CENTRALIZADOS EN LA RAÍZ
+      error_file: "./logs/api-error.log",
+      out_file: "./logs/api-out.log",
+      time: true, // Agrega la estampa de tiempo UTC a los logs
+
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
